a5b6e048be0ceeabd8557bc669e402bb43be6c3c
Wassim Chegham <github@wassim.app>
2018-11-25 22:15:52 +0000

    chore: expose the ConsoleLoggerService

--- projects/manekinekko/angular-web-bluetooth/src/public_api.ts
+++ projects/manekinekko/angular-web-bluetooth/src/public_api.ts
@@ -5,5 +5,6 @@
 export * from './lib/platform/browser';
 export * from './lib/platform/server';
 export * from './lib/bluetooth.service';
+export * from './lib/logger.service';
 export * from './lib/bluetooth.module';
 export * from './lib/lang/uuids';
