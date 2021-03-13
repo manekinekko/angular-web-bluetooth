f08bfa0483c3e063e3c42b78152a5b09282ceac9
Fayçal Tirich <faycal.tirich@renault.com>
2021-03-13 08:21:42 +0100

    test: do not test battery level ui implementation details

--- src/app/thingy52/battery-level.component.spec.ts
+++ src/app/thingy52/battery-level.component.spec.ts
@@ -1,15 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BluetoothCore, ConsoleLoggerService, NoLoggerService } from '@manekinekko/angular-web-bluetooth';
+import { render, screen } from '@testing-library/angular';
+import { createMock } from '@testing-library/angular/jest-utils';
 import { Subject } from 'rxjs';
-import {
-  BluetoothCore,
-  BrowserWebBluetooth,
-  ConsoleLoggerService,
-  FakeBluetoothDevice, NoLoggerService
-} from '@manekinekko/angular-web-bluetooth';
 import { BleService } from '../ble.service';
 import { BatteryLevelComponent } from './battery-level.component';
 
@@ -24,66 +21,43 @@ describe('BatteryLevelComponent', () => {
   const fakeValueFn = jest.fn((options) => fakeValue);
   const fakeDeviceDisconnectFn = jest.fn();
 
-  const fakeBluetoothCore: unknown = {
-    test: 'bob',
-    getDevice$() {
-      return fakeDevice;
-    },
-    streamValues$() {
-      return fakeStreamValue;
-    },
-    value$(options) {
-      return fakeValueFn(options);
-    },
-    disconnectDevice() {
-      fakeDeviceDisconnectFn();
-    }
-  };
+  const fakeBluetoothCore = createMock(BluetoothCore);
+  fakeBluetoothCore.getDevice$.mockImplementation(() => fakeDevice);
+  fakeBluetoothCore.streamValues$.mockImplementation(() => fakeStreamValue);
+  fakeBluetoothCore.value$.mockImplementation((options) => fakeValueFn(options));
+  fakeBluetoothCore.disconnectDevice.mockImplementation(() => fakeDeviceDisconnectFn());
+
   const fakeBleService = new BleService(fakeBluetoothCore as BluetoothCore);
 
-  beforeEach(() => {
-    TestBed
-      .configureTestingModule({
-        imports: [
-          MatSnackBarModule,
-          MatIconModule,
-          MatProgressSpinnerModule,
-          NoopAnimationsModule,
-        ],
-        declarations: [
-          BatteryLevelComponent,
-        ],
-      })
-      .overrideProvider(BleService, {useValue: fakeBleService})
-      .overrideProvider(BluetoothCore, {useValue: jest.fn()})
-      .overrideProvider(BrowserWebBluetooth, {useValue: jest.fn()})
-      .overrideProvider(ConsoleLoggerService, {useValue: new NoLoggerService()});
+  beforeEach(async () => {
+    const renderResult = await render(BatteryLevelComponent, {
+      imports: [
+        MatSnackBarModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        NoopAnimationsModule,
+      ],
+      componentProviders: [
+        {provide: BleService, useValue: fakeBleService},
+        {provide: ConsoleLoggerService, useValue: new NoLoggerService()},
+      ],
+    });
 
-    fixture = TestBed.createComponent(BatteryLevelComponent);
-    component = fixture.componentInstance;
+    fixture = renderResult.fixture;
+    component = renderResult.fixture.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeDefined();
-    expect(fixture).toMatchSnapshot();
-  });
 
-  it('should display device connected state', () => {
-    // given
-    component.ngOnInit();
+  it('should display device initial state', () => {
+    // when
     component.requestValue();
 
-    // when
-    fakeDevice.next(new FakeBluetoothDevice('1', 'My Device'));
-    fixture.detectChanges();
-
     // then
-    expect(fixture).toMatchSnapshot();
+    expect(screen.getByTestId('value').textContent).toBe('000%');
   });
 
   it('should display device service/characteristic value changes', () => {
     // given
-    component.ngOnInit();
     component.requestValue();
 
     // when
@@ -91,30 +65,26 @@ describe('BatteryLevelComponent', () => {
     const dataView = new DataView(new ArrayBuffer(8));
     dataView.setInt8(0, 99);
     fakeStreamValue.next(dataView);
+    fixture.detectChanges();
 
     // then
-    fixture.detectChanges();
-    const nativeElement: HTMLElement = fixture.nativeElement;
-
     expect(component.value).toEqual(99);
-    expect(nativeElement.querySelector('[data-testid="value"]').textContent).toEqual('99%');
+    expect(screen.getByTestId('value').textContent).toBe('99%');
 
     // when
     // 2 nd value change
     dataView.setInt8(0, 100);
     fakeStreamValue.next(dataView);
+    fixture.detectChanges();
 
     // then
-    fixture.detectChanges();
     expect(component.value).toEqual(100);
-    expect(nativeElement.querySelector('[data-testid="value"]').textContent).toEqual('100%');
+    expect(screen.getByTestId('value').textContent).toBe('100%');
   });
 
   it('should disconnect', () => {
     // given
-    component.ngOnInit();
     component.requestValue();
-    fakeDevice.next(new FakeBluetoothDevice('1', 'My Device'));
 
     // when
     component.disconnect();
