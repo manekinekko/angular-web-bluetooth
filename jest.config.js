59d07e7731ff394c4fa64a3187cb5eefd43dc45f
Wassim Chegham <github@wassim.dev>
2023-02-22 13:04:21 +0100

    fix: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  preset: 'jest-preset-angular',
   displayName: "angular-web-bluetooth-starter",
   setupFilesAfterEnv: ["<rootDir>/test-setup.ts"],
   modulePathIgnorePatterns: ["dist/*"],
+  globalSetup: 'jest-preset-angular/global-setup',
   moduleNameMapper: {
     "@manekinekko/(.+)": "<rootDir>/projects/manekinekko/$1/src/public_api.ts",
   },
